75bdbe71e2855c7f3ca3158ab55accd328bc8adc
Aday Mesa <adaymesagc@gmail.com>
2016-04-27 13:01:51 +0100

    Challenge 19 - Refactoring routes

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+	enable :sessions
   get '/' do
     erb(:index)
   end
 
   post "/names" do
-  	@player1_name = params[:player1_name]
-  	@player2_name = params[:player2_name]
+		session[:player1_name] = params[:player1_name]
+		session[:player2_name] = params[:player2_name] 	
+		redirect("/play")
+  end
+
+  get "/play" do
+  	@player1_name = session[:player1_name]
+  	@player2_name = session[:player2_name]
   	erb(:play)
   end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
