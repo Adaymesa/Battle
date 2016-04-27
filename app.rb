77b98e243495fa9bf51ab00713575a862d8dcb29
Aday Mesa <adaymesagc@gmail.com>
2016-04-27 16:13:39 +0100

    Challenge 22 completed - Player1 attacks Player2

--- app.rb
+++ app.rb
@@ -18,8 +18,12 @@ class Battle < Sinatra::Base
   	erb(:play)
   end
 
-
-
-  # start the server if ruby file executed directly
+  get "/attack" do
+  	@player1_name = session[:player1_name]
+  	@player2_name = session[:player2_name]
+  	@attacking = true
+  	erb(:play)
+  end
+# start the server if ruby file executed directly
   run! if app_file == $0
 end
