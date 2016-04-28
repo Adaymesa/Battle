4cfb92e14e2b409343e671252a63ca1e265015f5
Aday Mesa <adaymesagc@gmail.com>
2016-04-28 11:28:26 +0100

    Session properly removed

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class Battle < Sinatra::Base
   post "/names" do
 		$player1 = Player.new(params[:player1_name])
 		$player2 = Player.new(params[:player2_name])
-    session[:attacked] = false
+
 		redirect("/play")
   end
 
@@ -20,15 +20,16 @@ class Battle < Sinatra::Base
   	@player2_name = $player2.name
     @player1_hp = $player1.hit_points
     @player2_hp = $player2.hit_points 
-    @attacking = session[:attacked]
   	erb(:play)
   end
 
-  post "/attack" do
-    @player_attacked = params[:player] == 'Player1' ? $player1 : $player2
-    @execute_attack = Game.new.attack(@player_attacked)
-    session[:attacked] = true
-  	redirect('/play')
+  get "/attack" do
+  	@player1_name = $player1.name
+  	@player2_name = $player2.name
+    @player1_hp = $player1.hit_points
+    @player2_hp = $player2.hit_points 
+  	@player1_attack = Game.new.attack($player2)
+		erb(:play)
   end
 # start the server if ruby file executed directly
   run! if app_file == $0
